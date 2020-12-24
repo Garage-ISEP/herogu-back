14b0475a31941ae5b7bc57aea2c55347ba91ee8b
MrEthic <jeremiebasso@hotmail.fr>
2020-12-24 18:31:10 +0100

    Implements loggin with JWT

--- src/Controllers/Users.controller.ts
+++ src/Controllers/Users.controller.ts
@@ -1,30 +1,61 @@
-import { Controller, Param, Body, Get, Post, Patch, Delete, Redirect, HttpCode, OnNull, JsonController } from 'routing-controllers';
+import { Param, Body, Get, Post, Patch, Delete, Redirect, HttpCode, OnNull, JsonController, HttpError, UseBefore } from 'routing-controllers';
 import User from '../Models/DatabaseModels/User.model';
 import UserReq from '../Models/RequestModels/User.req.model'
+
 import * as bcrypt from 'bcrypt';
+
+import AdminMidleware from "../Services/MidleWares/AdminMidleWare";
+import JWTMidleware from "../Services/MidleWares/JWTMidleWare";
 
 import { Logger } from '../Utils/Logger.service';
 
 @JsonController()
 export class UserController {
 
-  private readonly log = new Logger(this);
+  private readonly _logger = new Logger(this);
 
   @Get('/users')
+  @UseBefore(JWTMidleware)
+  @UseBefore(AdminMidleware)
+  @OnNull(500)
   async getAll() {
-    return await User.findAll();
+    try {
+      const users = await User.findAll({ attributes: { exclude: ['hash_pswd'] } });
+      return JSON.stringify(users);
+    }
+    catch (e) {
+      this._logger.error(e);
+      return null;
+    }
   }
 
   @Get('/users/:studentId')
-  @OnNull(400)
+  @UseBefore(JWTMidleware)
+  @UseBefore(AdminMidleware)
+  @OnNull(500)
   async getOne(@Param('studentId') studentId: string) {
-    return await User.findOne({ where: { studentId } });
+    try {
+      const user = await User.findOne({ where: { studentId } })
+      return user !== null ? JSON.stringify(user) : new HttpError(400, "Invalid Id");
+    }
+    catch (e) {
+      this._logger.error(e);
+      return null
+    }
   }
 
   @OnNull(500)
   @Post('/users')
-  async post(@Body() user: UserReq) {
-    console.log(user);
+  async post(@Body({ required: true }) user: UserReq) {
+    try {
+      if (await User.count({ where: { studentId: user.student_id } }) !== 0) {
+        return new HttpError(400, "User with this studentId aleady exist");
+      }
+    }
+    catch (e) {
+      this._logger.error(e);
+      return null;
+    }
     const hashed_pswd = await bcrypt.hash(user.password, 10);
     const first_name = user.email.match(/^\w+/)[0] ?? "undefined";
     const last_name = user.email.match(/(?:\.)\w+/)[0].replace(/\./, "") ?? "undefined";
@@ -39,15 +70,15 @@ export class UserController {
       await new_user.save();
     }
     catch (e) {
-      this.log.error(e, user);
+      this._logger.error(e, user);
       return null
     }
-    this.log.info("")
+    this._logger.info("New user created : ", user);
     const verif_token = await bcrypt.hash(user.student_id + user.email, 10);
     return user;
   }
 
-  @Patch('/users/:id')
+  /* @Patch('/users/:id')
   patch(@Param('id') id: number, @Body() user: any) {
     return 'Updating a user...';
   }
@@ -55,5 +86,5 @@ export class UserController {
   @Delete('/users/:id')
   remove(@Param('id') id: number) {
     return 'Removing user...';
-  }
+  } */
 }
---
+++ src/Models/RequestModels/Auth.req.model.ts
@@ -0,0 +1,14 @@
+import { IsNotEmpty, MinLength, ValidateIf } from 'class-validator';
+
+export default class AuthReq {
+
+  @MinLength(8)
+  @IsNotEmpty()
+  @ValidateIf(password => /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/.test(password))
+  password: string;
+
+  @ValidateIf(stdId => /^\w{4}\d{5}$/.test(stdId))
+  @IsNotEmpty()
+  student_id: string;
+
+}
