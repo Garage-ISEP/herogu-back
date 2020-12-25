7694a2b971d5af6c4b73a5c5413191272b04ba25
MrEthic <jeremiebasso@hotmail.fr>
2020-12-25 19:41:52 +0100

    Improve auth system

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,12 @@
-import { createExpressServer } from 'routing-controllers';
+import { Action, createExpressServer, InternalServerError, UnauthorizedError } from 'routing-controllers';
 import "reflect-metadata";
 
 import { Sequelize } from 'sequelize-typescript';
 import { Dialect } from 'sequelize/types';
+
+import * as jwt from "jsonwebtoken";
+
+import { User, Role, Project } from "./Models/DatabaseModels";
 
 const sequelize = new Sequelize({
   logging: false,
@@ -14,13 +18,63 @@ const sequelize = new Sequelize({
   define: {
     schema: process.env.DB_SCHEMA ?? "herogu"
   },
-  models: [__dirname + '/Models/DatabaseModels']
+  models: [User, Role, Project]
 });
 
 
 createExpressServer({
   routePrefix: '/api',
   controllers: [__dirname + '/Controllers/*.js'],
+  async authorizationChecker(action: Action) {
+    const token = action.request.headers["auth"]
+    let jwtPayload: any;
+    // Read jwt token from header
+    try {
+      jwtPayload = await <any>jwt.verify(token, process.env.JWT_SECRET);
+    }
+    catch (e) {
+      throw new UnauthorizedError("Invalid token");
+    }
+
+    const uid: string = jwtPayload.uid;
+    let user: any;
+    // Get user by studentId from DB
+    try {
+      user = await User.findOne({ where: { studentId: uid }, include: [Role], attributes: { exclude: ['hash_pswd'] } });
+    }
+    catch (e) {
+      throw new InternalServerError("DB Failing");
+    }
+    // Check Role
+    try {
+      return user.role.name === "ADMIN";
+    }
+    catch (e) {
+      throw new InternalServerError("Can't retreive User from request");
+    }
+  },
+  async currentUserChecker(action: Action) {
+    const token = action.request.headers["auth"]
+    let jwtPayload: any;
+    // Read JWT token from header
+    try {
+      jwtPayload = await <any>jwt.verify(token, process.env.JWT_SECRET);
+    }
+    catch (e) {
+      throw new UnauthorizedError("Invalid token");
+    }
+
+    const uid: string = jwtPayload.uid;
+    let user: any;
+    // Get user by studentId
+    try {
+      user = await User.findOne({ where: { studentId: uid }, include: [Role], attributes: { exclude: ['hash_pswd'] } });
+      return user;
+    }
+    catch (e) {
+      throw new InternalServerError("DB Failing");
+    }
+  },
 }).listen(3000, async () => {
   await sequelize.sync({force:false});
   console.log("server running...");
