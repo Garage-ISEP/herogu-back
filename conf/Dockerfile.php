70b80f3e40939bc743f45effd72810442c7f5b21
Elariondakta <prevottheodore@gmail.com>
2021-01-13 23:22:52 +0100

    adding file signature for docker, disabling exec and enabling file upload

--- conf/Dockerfile.php
+++ conf/Dockerfile.php
@@ -4,6 +4,9 @@ RUN apt-get update -y && apt-get install -y libonig-dev
 
 RUN docker-php-ext-install -j$(nproc) pdo pdo_mysql mbstring mysqli exif && docker-php-ext-enable mysqli
 
+RUN a2enmod rewrite
+RUN a2disconf serve-cgi-bin
+
 COPY php.ini /etc/php/php.ini
 
 LABEL ORIGIN="garage"
