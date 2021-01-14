4b1a94b1e232bad5b6edf8aef1605702734e1965
Elariondakta <prevottheodore@gmail.com>
2021-01-14 05:01:18 +0100

    updating last mail field && return httpcode

--- src/Controllers/Users.controller.ts
+++ src/Controllers/Users.controller.ts
@@ -94,6 +94,8 @@ export class UserController {
     const verifCode = await bcrypt.hash(user.studentId, 10);
     try {
       mailer.sendVerificationMail(user.mail, verifCode);
+      user.set("last_mail", new Date());
+      return HttpCode(200);
     }
     catch (e) {
       this._logger.error(e, user);
