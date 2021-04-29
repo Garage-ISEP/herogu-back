8c004653e9a9f7e2828340099ffd16a4500dc9ad
totodore <prevottheodore@gmail.com>
2021-04-29 23:51:03 +0100

    adding database configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { AppLogger } from './utils/app-logger.util';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from "@nestjs/config";
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot(), //env configuration
-    AppLogger //Custom logger
+    AppLogger, //Custom logger
+    TypeOrmModule.forRoot({   //Database configuration
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PSWD,
+      database: process.env.DB_NAME,
+      schema: process.env.DB_SCHEMA,
+      entities: [],
+      synchronize: process.env.NODE_ENV === "dev",
+    })
   ],
   controllers: [],
   providers: [],
