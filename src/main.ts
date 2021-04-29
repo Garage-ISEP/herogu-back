e91c37aa7a7b072c9bc9252ad332b0e785e6ba55
totodore <prevottheodore@gmail.com>
2021-04-29 21:19:52 +0100

    initialize swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   app.enableCors();
+  const config = new DocumentBuilder()
+    .setTitle('ATMO-API')
+    .setVersion('1.0')
+    .setDescription("Api reliant le système de BDD et de dockers de herogu vers son dashboard Angular")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
