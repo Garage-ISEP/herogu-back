eca4d65344fc742bbf5c0abe4861fc7c46b8f1f8
Totodore <prevottheodore@gmail.com>
2021-09-11 20:19:50 +0200

    refactoring and adding update password view

--- src/controllers/auth/auth.controller.ts
+++ src/controllers/auth/auth.controller.ts
@@ -1,13 +1,12 @@
 import { AuthGuard } from './../../guards/auth.guard';
-import { BadRequestException, Body, Controller, ForbiddenException, Post, UseGuards } from '@nestjs/common';
+import { BadRequestException, Body, Controller, ForbiddenException, Get, Patch, Post, UseGuards } from '@nestjs/common';
 import { User } from 'src/database/user.entity';
-import { LoginDto } from './login.dto';
+import { LoginDto, LoginResponse, RegisterDto, UpdatePasswordDto } from './auth.dto';
 import * as bcrypt from "bcrypt";
 import * as jwt from "jsonwebtoken";
 import { CurrentUser } from 'src/decorators/current-user.decorator';
 import { ToManyResendMailException } from 'src/errors/auth.exception';
 import { MailerService } from 'src/services/mailer.service';
-import { RegisterDto } from './register.dto';
 @Controller('auth')
 export class AuthController {
 
@@ -15,15 +14,21 @@ export class AuthController {
     private readonly _mail: MailerService,
   ) { }
 
+  @Get("me")
+  @UseGuards(AuthGuard)
+  public async getUser(@CurrentUser() user: User): Promise<User> {
+    return user;
+  }
+
   @Post('login')
-  public async login(@Body() creds: LoginDto): Promise<{ token: string }> {
+  public async login(@Body() creds: LoginDto): Promise<LoginResponse> {
     const user = await User.findOne({ where: { studentId: creds.studentId } });
     if (!user)
       throw new BadRequestException("This user does not exists");
     if (!await bcrypt.compare(creds.password, user.password))
       throw new ForbiddenException("Invalid password");
 
-    return { token: jwt.sign(user.studentId, user.id) };
+    return new LoginResponse(jwt.sign(user.studentId, user.id), user);
   }
 
   @Post('register')
@@ -60,5 +65,15 @@ export class AuthController {
       throw new BadRequestException("User does not exists");
     };
     await User.update(user.id, { verified: true });
+  }
+
+  @Patch('password')
+  @UseGuards(AuthGuard)
+  public async updatePassword(@CurrentUser() user: User, @Body() body: UpdatePasswordDto): Promise<void> {
+    if (!await bcrypt.compare(body.password, body.newPassword))
+      throw new ForbiddenException("Invalid password");
+    if (body.password === body.newPassword)
+      throw new BadRequestException("Passwords are equals");
+    await User.update(user.id, { password: await bcrypt.hash(body.newPassword, 10) });
   }
 }
